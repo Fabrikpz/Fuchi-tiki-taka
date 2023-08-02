18f9b6fc3e13279ff34453714de19e29e290e07c
Fabrikpz <fabrisilveyra@gmail.com>
2023-08-02 18:56:07 -0300

    agregue input y modifique un toke el css

--- controllers/myController.js
+++ controllers/myController.js
@@ -1,11 +1,11 @@
 const OneModel = require('../models/myModel');
 
 exports.inicio = (req, res) => {
-    const topTeams = ['Manester City', 'Manchester City', 'PSG'];
-    const leftTeams = ['Real Vardrid', 'Bayern', 'Milan'];
+    const topTeams = ['Manchester United', 'Manchester City', 'PSG'];
+    const leftTeams = ['Real Madrid', 'Bayern', 'Liverpool'];
     const players = [
-        'Jugador 1', 'Jugador 2', 'Jugador 3', 'Jugador 4',
-        'Jugador 5', 'Jugador 6', 'Jugador 7', 'Jugador8', 'Jugador9'];
+        'Di Maria', 'Robinho', 'Navas', 'Schweinsteiger',
+        'Cancelo', 'Coman', 'Owen', 'Sterling', 'Wijnaldum'];
 
     res.render('panel', { topTeams, leftTeams, players});
 };
